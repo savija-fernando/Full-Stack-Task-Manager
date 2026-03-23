df215aea2f9376b33cd8ccd3b28e18b71cfa1ac6
savija-fernando <savijanethika@gmail.com>
2026-03-23 22:47:32 +0530

    services

---
+++ frontend/src/services/taskService.ts
@@ -0,0 +1,30 @@
+import axios from "axios";
+import type { Task, CreateTaskDto, UpdateTaskDto } from "../types/task";
+
+const API_URL = "https://localhost:5224/api/tasks"; 
+
+export const getTasks = async (): Promise<Task[]> => {
+  const response = await axios.get<Task[]>(API_URL);
+  return response.data;
+};
+
+export const getTaskById = async (id: number): Promise<Task> => {
+  const response = await axios.get<Task>(`${API_URL}/${id}`);
+  return response.data;
+};
+
+export const createTask = async (task: CreateTaskDto): Promise<Task> => {
+  const response = await axios.post<Task>(API_URL, task);
+  return response.data;
+};
+
+export const updateTask = async (
+  id: number,
+  task: UpdateTaskDto
+): Promise<void> => {
+  await axios.put(`${API_URL}/${id}`, task);
+};
+
+export const deleteTask = async (id: number): Promise<void> => {
+  await axios.delete(`${API_URL}/${id}`);
+};
